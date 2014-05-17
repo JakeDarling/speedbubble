05eba42f6e7bb313a6c2025fbb8fdd08184c01b5
Jake Darling <jake.darling1@gmail.com>
2014-05-17 18:47:26 -0400

    initial commit

---
+++ desktop/src/com/speedbubble/jakedean/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.speedbubble.jakedean.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.speedbubble.jakedean.SpeedBubble;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpeedBubble(), config);
+	}
+}
