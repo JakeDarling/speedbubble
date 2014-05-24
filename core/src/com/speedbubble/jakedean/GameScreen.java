e53917a61ca7eae6e298b17fbbbb1d6eb7c51d05
Dean Mondy <djm6386@g.rit.edu>
2014-05-24 02:18:25 -0400

    rearranged gamescreen until state machine

--- core/src/com/speedbubble/jakedean/GameScreen.java
+++ core/src/com/speedbubble/jakedean/GameScreen.java
@@ -3,6 +3,7 @@ package com.speedbubble.jakedean;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.speedbubble.jakedean.MainMenuScreen.Mode;
 
@@ -17,6 +18,13 @@ public class GameScreen implements Screen {
     private float timeLeft;
     private float stateTime;
     private int score;
+    private Texture background;
+    
+    private enum GameState{
+    	START, RUNNING, PAUSED, RESTART, RESUME, QUIT, GAME_OVER
+    }
+    
+    GameState state;
 
     public GameScreen(SpeedBubble s, Mode m) {
         sb = s;
@@ -24,79 +32,164 @@ public class GameScreen implements Screen {
         timeLeft = 15f;
         popStarted = false;
         batch = new SpriteBatch();
-    }
-
-    private void update() {
-        stateTime += Gdx.graphics.getDeltaTime();
-        timeLeft -= Gdx.graphics.getDeltaTime();
-
-        //mode.update();
-        if (Gdx.input.justTouched()){
-            if(Gdx.input.getX()>=Assets.bubble.getX() && Gdx.input.getX() <= Assets.bubble.getX()+Assets.bubble.getWidth()
-                    && ((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) >= Assets.bubble.getY() && ((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) <= Assets.bubble.getY()+Assets.bubble.getHeight()){
-                stateTime=0;
-                Assets.previousBubble.setPosition(Assets.bubble.getX(), Assets.bubble.getY());
-                popStarted=true;
-                Assets.relocateBubble();
-                Assets.bubbleSound.play();
-                score++;
-            }
-            else{ sb.setScreen(new FailureScreen(sb, score)); }
-        }
-    }
+        state = GameState.START;
+        
+        background = new Texture(Gdx.files.internal("mainMenuBackground.png"));
+    }   
+    
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,1,1,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        Assets.currentBubble = Assets.poppingBubble.getKeyFrame(stateTime, false);
-
-
         update();
+        draw();
+    }
+    
+    
+    
+    private void update() {
+    	
+        switch(state){
+		case START:
+			updateStart();
+			break;
+		case RUNNING:
+			updateRunning();
+			break;
+//		case PAUSED:
+//			updatePaused();
+//			break;
+//		case RESTART:
+//			updateRestart();
+//			break;
+//		case RESUME:
+//			updateResume();
+//			break;
+//		case QUIT:
+//			updateQuit();
+//			break;
+//		case GAME_OVER:
+//			updateGameOver();
+//			break;
+		}
+    }
+    private void updateStart(){
+    	if (Gdx.input.justTouched()){
+    		state = GameState.RUNNING;
+    	}
+    }
+    private void updateRunning(){
+    	
+        //mode.update(); TODO
+    	
+        stateTime += Gdx.graphics.getDeltaTime();
+        timeLeft -= Gdx.graphics.getDeltaTime();
+        
+    	 if (Gdx.input.justTouched()){
+             if(Gdx.input.getX()>=Assets.bubble.getX() && Gdx.input.getX() <= Assets.bubble.getX()+Assets.bubble.getWidth()
+                     && ((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) >= Assets.bubble.getY() && ((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) <= Assets.bubble.getY()+Assets.bubble.getHeight()){
+                 stateTime=0;
+                 Assets.previousBubble.setPosition(Assets.bubble.getX(), Assets.bubble.getY());
+                 popStarted=true;
+                 Assets.relocateBubble();
+                 Assets.bubbleSound.play();
+                 score++;
+             }
+             else{ sb.setScreen(new FailureScreen(sb, score)); }
+         }
+    	 
+    	 if (timeLeft < 0){
+    		 dispose();
+    		 sb.setScreen(new GameOverScreen(sb, score)); 
+    	 }
+    }
+    
+    
+    
+    
+    private void draw(){
+    	switch(state){
+ 		case START:
+ 			drawStart();
+ 			break;
+ 		case RUNNING:
+ 			drawRunning();
+ 			break;
+// 		case PAUSED:
+// 			drawPaused();
+// 			break;
+// 		case RESTART:
+// 			drawRestart();
+// 			break;
+// 		case RESUME:
+// 			drawResume();
+// 			break;
+// 		case QUIT:
+// 			drawQuit();
+// 			break;
+// 		case GAME_OVER:
+// 			drawGameOver();
+// 			break;
+ 		}
+    }
+    private void drawStart(){
+    	Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
         batch.begin();
+        batch.draw(background, Gdx.graphics.getWidth() - 1024, Gdx.graphics.getHeight() - 512);
+        Assets.bubbleFontBig.draw(batch, "TAP ANYWHERE TO BEGIN", (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("TAP ANYWHERE TO BEGIN").width) / 2,
+        		Gdx.graphics.getHeight()/2 + Assets.bubbleFontBig.getBounds("TAP ANYWHERE TO BEGIN").height);
+        batch.end();
+    }
+    private void drawRunning(){
+    	Assets.currentBubble = Assets.poppingBubble.getKeyFrame(stateTime, false);
+    	
+    	Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+         
+         
+    	batch.begin();
+    	batch.draw(background, Gdx.graphics.getWidth() - 1024, Gdx.graphics.getHeight() - 512);
         if(popStarted) drawAnimation();
         Assets.bubble.draw(batch);
-        Assets.bubbleFontSmall.draw(batch, "BUBBLES POPPED: " + score, 0,
-                Gdx.graphics.getHeight());
-        Assets.bubbleFontSmall.draw(batch, "TIME REMAINING: " + String.format("%.2f", timeLeft), (Gdx.graphics.getWidth() - Assets.bubbleFontSmall.getBounds("TIME REMAINING:  ").width)/2,
-                Gdx.graphics.getHeight());
+        Assets.bubbleFontBig.draw(batch, "BUBBLES POPPED: " + score, 0,
+        		Assets.bubbleFontBig.getBounds("BUBBLES POPPED:  ").height + 10);
+        Assets.bubbleFontBig.draw(batch, "TIME REMAINING: " + String.format("%.2f", timeLeft), 
+        		(Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("TIME REMAINING:  ").width)/2,   Gdx.graphics.getHeight());
         batch.end();
-        if (timeLeft < 0){ sb.setScreen(new GameOverScreen(sb, score)); }
     }
-
-
+    
+    
+    
     public void drawAnimation(){
         Assets.currentBubble = Assets.poppingBubble.getKeyFrame(stateTime, false);
         batch.draw(Assets.currentBubble, Assets.previousBubble.getX(), Assets.previousBubble.getY(), Assets.previousBubble.getWidth(), Assets.previousBubble.getHeight());
     }
+    
+    
+    
+    @Override
+    public void dispose() {
+
+    }
+    
+    
 
     @Override
     public void resize(int width, int height) {
 
     }
-
     @Override
     public void show() {
-
     }
-
     @Override
     public void hide() {
-
     }
-
     @Override
     public void pause() {
 
     }
-
     @Override
     public void resume() {
-
-    }
-
-    @Override
-    public void dispose() {
 
     }
 }
--- core/src/com/speedbubble/jakedean/SpeedBubble.java
+++ core/src/com/speedbubble/jakedean/SpeedBubble.java
@@ -1,9 +1,6 @@
 package com.speedbubble.jakedean;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SpeedBubble extends Game {
 
