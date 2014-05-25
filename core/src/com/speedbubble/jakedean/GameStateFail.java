d9f8a7bd7ea9077d4367722ace24c5d14fbd829d
Dean Mondy <djm6386@g.rit.edu>
2014-05-25 14:02:31 -0400

    added options to fail and game over screens

--- core/src/com/speedbubble/jakedean/GameStateFail.java
+++ core/src/com/speedbubble/jakedean/GameStateFail.java
@@ -23,12 +23,25 @@ public class GameStateFail implements GameState {
                 Gdx.graphics.getHeight()/2 + (float) 2.5*Assets.bubbleFontBig.getBounds("FAILURE").height);
         Assets.bubbleFontBig.draw(batch, "FINAL SCORE: " + score, (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("FINAL SCORE:  ").width)/2,
                 Gdx.graphics.getHeight()/2 + Assets.bubbleFontBig.getBounds("FINAL SCORE:  ").height/2);
+        Assets.bubbleFontBig.draw(batch, "MAIN MENU", (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("MAIN MENU").width)/4,
+                (float)2.5*Assets.bubbleFontBig.getBounds("MAIN MENU").height);
+        Assets.bubbleFontBig.draw(batch, "RETRY", (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("RETRY").width)*3/4,
+                (float)2.5*Assets.bubbleFontBig.getBounds("RETRY").height);
         batch.end();
     }
 
 	@Override
 	public void update(GameScreen screen, float deltaTime) {
-		if (Gdx.input.justTouched()){
+		if (Gdx.input.justTouched() && Gdx.input.getX() > (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("MAIN MENU").width)/4 &&
+				Gdx.input.getX() < (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("MAIN MENU").width)/4 + Assets.bubbleFontBig.getBounds("MAIN MENU").width &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) > (float)2.5*Assets.bubbleFontBig.getBounds("MAIN MENU").height - Assets.bubbleFontBig.getBounds("MAIN MENU").height &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) < (float)2.5*Assets.bubbleFontBig.getBounds("MAIN MENU").height){
+    		screen.getGame().setScreen(new MainMenuScreen(screen.getGame()));
+    	}
+		if (Gdx.input.justTouched() && Gdx.input.getX() > (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("RETRY").width)*3/4 &&
+				Gdx.input.getX() < (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("RETRY").width)*3/4 + Assets.bubbleFontBig.getBounds("RETRY").width &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) > (float)2.5*Assets.bubbleFontBig.getBounds("RETRY").height - Assets.bubbleFontBig.getBounds("RETRY").height &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) < (float)2.5*Assets.bubbleFontBig.getBounds("RETRY").height){
 			screen.reset();
     	}
 	}
--- core/src/com/speedbubble/jakedean/GameStateOver.java
+++ core/src/com/speedbubble/jakedean/GameStateOver.java
@@ -23,13 +23,26 @@ public class GameStateOver implements GameState {
                 Gdx.graphics.getHeight()/2 + (float)2.5*Assets.bubbleFontBig.getBounds("GAME OVER").height);
         Assets.bubbleFontBig.draw(batch, "FINAL SCORE: " + score, (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("FINAL SCORE:  ").width)/2,
                 Gdx.graphics.getHeight()/2 + Assets.bubbleFontBig.getBounds("FINAL SCORE:  ").height/2);
+        Assets.bubbleFontBig.draw(batch, "MAIN MENU", (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("MAIN MENU").width)/4,
+                (float)2.5*Assets.bubbleFontBig.getBounds("MAIN MENU").height);
+        Assets.bubbleFontBig.draw(batch, "RETRY", (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("RETRY").width)*3/4,
+                (float)2.5*Assets.bubbleFontBig.getBounds("RETRY").height);
         batch.end();
     }
 
 	@Override
 	public void update(GameScreen screen, float deltaTime) {
-		if (Gdx.input.justTouched()){
-    		screen.getGame().setScreen(new MainMenuScreen(screen.getGame()));;
+		if (Gdx.input.justTouched() && Gdx.input.getX() > (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("MAIN MENU").width)/4 &&
+				Gdx.input.getX() < (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("MAIN MENU").width)/4 + Assets.bubbleFontBig.getBounds("MAIN MENU").width &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) > (float)2.5*Assets.bubbleFontBig.getBounds("MAIN MENU").height - Assets.bubbleFontBig.getBounds("MAIN MENU").height &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) < (float)2.5*Assets.bubbleFontBig.getBounds("MAIN MENU").height){
+    		screen.getGame().setScreen(new MainMenuScreen(screen.getGame()));
+    	}
+		if (Gdx.input.justTouched() && Gdx.input.getX() > (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("RETRY").width)*3/4 &&
+				Gdx.input.getX() < (Gdx.graphics.getWidth() - Assets.bubbleFontBig.getBounds("RETRY").width)*3/4 + Assets.bubbleFontBig.getBounds("RETRY").width &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) > (float)2.5*Assets.bubbleFontBig.getBounds("RETRY").height - Assets.bubbleFontBig.getBounds("RETRY").height &&
+				((Gdx.input.getY() - Gdx.graphics.getHeight()) * -1) < (float)2.5*Assets.bubbleFontBig.getBounds("RETRY").height){
+			screen.reset();
     	}
 	}
     
